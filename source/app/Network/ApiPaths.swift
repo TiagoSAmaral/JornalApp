59a2a741368c8dec9c99aed7217b95cf04abe22c
Tiago da Silva Amaral <tiagofly@outlook.com>
2018-08-15 16:02:18 -0300

    Criado arquivo para definir a URL base dos ambientes de release e teste, e vairaveis lazyload para criação das URLs.

---
+++ source/app/Network/ApiPaths.swift
@@ -0,0 +1,33 @@
+//
+//  APIPaths.swift
+//  jornalapp
+//
+//  Created by Tiago da Silva Amaral on 15/08/18.
+//  Copyright © 2018 com.outlook.tiagofly. All rights reserved.
+//
+
+import Foundation
+
+protocol ApiPathsInterface {
+
+	var urlNoveltyList: URL { get }
+	var urlNovely: URL { get }
+}
+
+class ApiPaths: ApiPathsInterface {
+
+	fileprivate let enviroment: Environment = Environment()
+	fileprivate var urlBase: String!
+
+	init() {
+
+		switch enviroment.current {
+		case .debug:
+			self.urlBase = "http://localhost:3001"
+		case .release:
+			self.urlBase = " https://raw.githubusercontent.com"
+		}
+	}
+
+	var urlNoveltyList: URL { return  URL(string: "\(self.urlBase!)/Infoglobo/desafio-apps/master/capa.json")! }
+}
