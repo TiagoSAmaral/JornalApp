27fbe638eabd1dbdd2d71f7842d9e4305af2affa
Tiago da Silva Amaral <tiagofly@outlook.com>
2018-08-15 21:39:28 -0300

    Implementado elemento de banner

---
+++ source/app/News/components/banner/Banner.swift
@@ -0,0 +1,46 @@
+//
+//  Banner.swift
+//  jornalapp
+//
+//  Created by Tiago da Silva Amaral on 15/08/18.
+//  Copyright © 2018 com.outlook.tiagofly. All rights reserved.
+//
+
+import UIKit
+import Kingfisher
+
+class Banner: UIView {
+
+	@IBOutlet var photo: UIImageView!
+	@IBOutlet var title: UILabel!
+	@IBOutlet var section: UILabel!
+
+	override func draw(_ rect: CGRect) {
+		// Drawing code
+
+		self.section.isHidden = true
+		self.setupView(of: self.section, fontName: "Arial Rounded MT Bold", size: 14.0, colorFont: .white, fitContent: true, alignment: .center, backgroundColor: .black)
+		self.setupView(of: self.title, fontName: "Arial Rounded MT Bold", size: 16.0, colorFont: .white, fitContent: false, alignment: .center, backgroundColor: .clear)
+		self.photo.layer.addSublayer(self.addGradientShadow(with: self.photo, height: 0.5)!)
+	}
+
+	func setBanner(with notice: Novelty) {
+
+		self.photo.kf.setImage(with: notice.imagens![0].sourceUrl!, placeholder: UIImage(named: "cover-default")!)
+		self.section.text = notice.secao!.nome!
+		self.title.text = notice.titulo
+		self.section.isHidden = false
+	}
+
+	func setupView(of label: UILabel, fontName: String, size: CGFloat, colorFont: UIColor, fitContent: Bool, alignment: NSTextAlignment, backgroundColor: UIColor) {
+
+		label.backgroundColor = backgroundColor
+		label.textColor = colorFont
+		label.font = UIFont(name: fontName, size: size)
+		label.textAlignment = alignment
+
+		if fitContent {
+			label.sizeToFit()
+		}
+	}
+}
