b0d7da964464d3bcf9636a020af502778d299b29
Tiago da Silva Amaral <tiagofly@outlook.com>
2018-08-16 19:01:29 -0300

    Implementado subclasse de UITableView -> NewsTableView para gerenciar lista de notificas.

---
+++ source/app/News/components/list/NewsTableView.swift
@@ -0,0 +1,47 @@
+//
+//  NewsTableView.swift
+//  jornalapp
+//
+//  Created by Tiago da Silva Amaral on 15/08/18.
+//  Copyright © 2018 com.outlook.tiagofly. All rights reserved.
+//
+
+import UIKit
+
+class NewsTableView: UITableView{
+
+	var presenter: NewsInterface!
+
+	override func draw(_ rect: CGRect) {
+		// Drawing code
+
+		self.dataSource = self
+		self.delegate = self
+		self.estimatedRowHeight = 60.0
+
+		self.registerCell()
+	}
+
+	func registerCell(){
+		self.register(UINib(nibName: NoveltyCell.identifier, bundle: nil), forCellReuseIdentifier: NoveltyCell.identifier)
+	}
+}
+
+extension NewsTableView: UITableViewDataSource {
+
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return self.presenter.sizeList
+	}
+
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		return self.presenter.buildCell(at: indexPath, inTo: tableView)
+	}
+
+}
+
+extension NewsTableView: UITableViewDelegate {
+	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		tableView.deselectRow(at: indexPath, animated: true)
+		self.presenter!.selectCell(at: indexPath.row)
+	}
+}
