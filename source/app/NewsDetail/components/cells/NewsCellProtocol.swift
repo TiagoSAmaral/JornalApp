e205a4a999612245da479710a85645c8c3d3412d
Tiago da Silva Amaral <tiagofly@outlook.com>
2018-08-17 14:46:26 -0300

    Implementado NewsDetailTitleCell

---
+++ source/app/NewsDetail/components/cells/NewsCellProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  NewsCellProtocol.swift
+//  jornalapp
+//
+//  Created by Tiago da Silva Amaral on 17/08/18.
+//  Copyright © 2018 com.outlook.tiagofly. All rights reserved.
+//
+
+import Foundation
+
+protocol NewsCellProtocol {
+
+	func setCell(info: Novelty)
+}
---
+++ source/app/NewsDetail/components/cells/NewsTitleCell/NewsTitleCell.swift
@@ -0,0 +1,28 @@
+//
+//  NewsTitleCell.swift
+//  jornalapp
+//
+//  Created by Tiago da Silva Amaral on 17/08/18.
+//  Copyright © 2018 com.outlook.tiagofly. All rights reserved.
+//
+
+import UIKit
+
+class NewsTitleCell: UITableViewCell, NewsCellProtocol {
+
+	@IBOutlet weak var titleLabel: UILabel!
+	override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+	func setCell(info: Novelty) {
+		self.titleLabel.text = info.titulo?.uppercased()
+	}
+}
