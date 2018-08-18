c5ab4e968de515377bf1b971bed649f9f2513eaa
Tiago da Silva Amaral <tiagofly@outlook.com>
2018-08-18 17:50:32 -0300

    Separado enum MapNoveltyBody

---
+++ source/app/NewsDetail/MapNoveltyBody.swift
@@ -0,0 +1,13 @@
+//
+//  MapNoveltyBody.swift
+//  jornalapp
+//
+//  Created by Tiago da Silva Amaral on 18/08/18.
+//  Copyright © 2018 com.outlook.tiagofly. All rights reserved.
+//
+
+import Foundation
+
+enum MapNovelyBody {
+	case title, subtitle, info, image, text
+}
