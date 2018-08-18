af8e1e813cd937f3d1e309030685228ac02b6eca
Tiago da Silva Amaral <tiagofly@outlook.com>
2018-08-17 23:11:56 -0300

    Implementado celula para texto completo da noticia.

---
+++ source/app/NewsDetail/components/cells/NewsTextCell/NewsTextCell.swift
@@ -0,0 +1,29 @@
+//
+//  NewsTextCell.swift
+//  jornalapp
+//
+//  Created by Tiago da Silva Amaral on 17/08/18.
+//  Copyright © 2018 com.outlook.tiagofly. All rights reserved.
+//
+
+import UIKit
+
+class NewsTextCell: UITableViewCell, NewsCellProtocol {
+
+	@IBOutlet weak var textContent: UILabel!
+
+	override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+	func setCell(info: Novelty) {
+		self.textContent.text = info.texto
+	}
+}
